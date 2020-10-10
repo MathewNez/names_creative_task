f5a7d3cd1a63ad286581dead1b17431f5a67a26b
Mathew Neznaeff <matveyneznaev@mail.ru>
2020-10-10 21:09:22 +0500

    Initial commit

---
+++ HeatmapTask.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using ZedGraph;
+
+/*
+ * !!!ВНИМАНИЕ!!!
+ * если ещё не поздно, то тут будет ссылка на github-репу,
+ * куда я выложу решение творческого задания, возможно,
+ * даже с адекватными и последовательными коммитами
+ * ССЫЛКА: https://github.com/.../ (пока её нет, но всё впереди)
+ */
+
+namespace Names
+{
+    internal static class HeatmapTask
+    {
+
+        public static void InitArray(string[] arr, int shift)
+        {
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                arr[i] = (i + shift).ToString();
+            }
+        }
+        public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+        {
+            var daysX = new string[30];
+            var monthsY = new string[12];
+            InitArray(daysX, 2);
+            InitArray(monthsY, 1);
+            var count = new double[30, 12];
+            foreach (var person in names)
+            {
+                if (person.BirthDate.Day != 1)
+                {
+                    ++count[person.BirthDate.Day-2, person.BirthDate.Month-1];
+                }
+            }
+            return new HeatmapData(
+                "Рождаемость в зависимости от дня и месяца",
+                count, 
+                daysX, 
+                monthsY);
+        }
+    }
+}
---
+++ HistogramTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace Names
+{
+    internal static class HistogramTask
+    {
+        public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+        {
+            var dateX = new string[31];
+            var countY = new double[31];
+            for (int i = 0; i < dateX.Length; ++i)
+            {
+                dateX[i] = (i + 1).ToString();
+            }
+            foreach (var person in names)
+            {
+                if (person.BirthDate.Day != 1 && person.Name == name)
+                {
+                    ++countY[person.BirthDate.Day - 1];
+                }
+            }
+            return new HistogramData(
+                string.Format("Рождаемость людей с именем '{0}'", name), 
+                dateX, 
+                countY);
+        }
+    }
+}
